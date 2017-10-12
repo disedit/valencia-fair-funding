526577d5e296f6bafd5beffd650e68f5d3877935
Guillem <guillem@disedit.com>
2017-10-12 22:34:41 +0200

    Add FontAwesome 5

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,4 +3,7 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   @php(wp_head())
+  <script defer src="{{ get_bloginfo('stylesheet_directory') . '/fontawesome/packs/regular.min.js' }}"></script>
+  <script defer src="{{ get_bloginfo('stylesheet_directory') . '/fontawesome/packs/brands.min.js' }}"></script>
+  <script defer src="{{ get_bloginfo('stylesheet_directory') . '/fontawesome/fontawesome.min.js' }}"></script>
 </head>
