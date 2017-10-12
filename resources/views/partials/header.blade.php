98d8bf5ed9182adc7d442acb9c1083f1a13d510c
Guillem <guillem@disedit.com>
2017-10-12 19:02:38 +0200

    Initial commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,28 @@
+<header class="container-fluid top">
+  <div class="row">
+    <div class="col-5">
+      Logos
+    </div>
+    <div class="col-5">
+      Languages
+    </div>
+  </div>
+</header>
+
+<header class="banner">
+  <div class="container">
+
+  </div>
+</header>
+
+<?php /*
+
+<nav class="nav-primary">
+  @if (has_nav_menu('primary_navigation'))
+    {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+  @endif
+</nav>
+
+*/
+
+?>
