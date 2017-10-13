e2b292a0f94ebe190e6cdde41eefcb7bedf2f453
Guillem <guillem@disedit.com>
2017-10-13 10:49:59 +0200

    Add form styling

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,6 +1,14 @@
 export default {
   init() {
-    // JavaScript to be fired on all pages
+    $(".main-input input").on('focus', function() {
+      $(this).parent().addClass('main-input-active');
+    });
+
+    $(".main-input input").on('blur', function() {
+      if(!$(this).val()) {  
+        $(this).parent().removeClass('main-input-active');
+      }
+    });
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
