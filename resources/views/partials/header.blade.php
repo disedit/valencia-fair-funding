948d4fe3f197fedbad29d7d647441bd67f9fe9e7
Guillem <guillem@disedit.com>
2017-10-13 18:09:37 +0200

    Move nav outside of header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,54 +1,4 @@
-<header class="top">
-  <div class="container-fluid">
-    <div class="organizers d-none d-lg-block">
-      <ul>
-        <li class="ugt"><a href=""><img src="@asset('images/ugt.png')" alt="UGT" /></a></li>
-        <li class="ccoo"><a href=""><img src="@asset('images/ccoo.png')" alt="CCOO" /></a></li>
-        <li class="psoe"><a href=""><img src="@asset('images/psoe.png')" alt="PSOE" /></a></li>
-        <li class="compromis"><a href=""><img src="@asset('images/compromis.png')" alt="Compromís" /></a></li>
-        <li class="podemos"><a href=""><img src="@asset('images/podemos.png')" alt="Podemos" /></a></li>
-      </ul>
-    </div>
-
-    <div class="hashtag elem-hidden">
-      <h1><a href="">#FinançamentJust</a></h1>
-    </div>
-
-    <div class="menu">
-      <div class="menu-items">
-        <ul class="d-flex d-lg-flex">
-          <li class="d-lg-none">
-            <a href="">Manifestació</a>
-          </li>
-          <li class="d-lg-none">
-            <a href="">Manifest</a>
-          </li>
-          <li class="d-lg-none">
-            <a href="">Notes de premsa</a>
-          </li>
-          <li class="d-lg-none">
-            <a href="">Contacta</a>
-          </li>
-          <li>
-            <div class="fb-like" data-href="http://financamentjust.com" data-layout="button_count" data-action="like" data-size="large" data-show-faces="false" data-share="false"></div>
-          </li>
-          <li>
-            <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-text="dfgfdg" data-url="http://fincancamentjust.com" data-hashtags="FinançamentJust" data-show-count="false">Tweet</a>
-          </li>
-          <li class="active">
-            <a href="">Valencià</a>
-          </li>
-          <li>
-            <a href="">Castellano</a>
-          </li>
-        </ul>
-      </div>
-      <ul class="menu-trigger d-lg-none">
-        <li><a href=""><i class="far fa-bars"></i> <span class="sr-only">Menú</span></a></li>
-      </ul>
-    </div>
-  </div>
-</header>
+@include('partials.nav')
 
 <header class="container-fluid banner">
   <div class="row">
@@ -57,20 +7,10 @@
         <span class="upper-slogan">{{ __('Per un', 'fair-funding') }}</span>
         <span class="main-slogan">{{ __('Finançament Just', 'fair-funding') }}</span>
       </h1>
+
+      @include('partials.metro')
   </div>
   <div class="map col-lg-3 d-none d-lg-block">
     Map
   </div>
 </header>
-
-<?php /*
-
-<nav class="nav-primary">
-  @if (has_nav_menu('primary_navigation'))
-    {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-  @endif
-</nav>
-
-*/
-
-?>
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,51 @@
+<nav class="top">
+  <div class="container-fluid">
+    <div class="organizers d-none d-lg-block">
+      <ul>
+        <li class="ugt"><a href=""><img src="@asset('images/ugt.png')" alt="UGT" /></a></li>
+        <li class="ccoo"><a href=""><img src="@asset('images/ccoo.png')" alt="CCOO" /></a></li>
+        <li class="psoe"><a href=""><img src="@asset('images/psoe.png')" alt="PSOE" /></a></li>
+        <li class="compromis"><a href=""><img src="@asset('images/compromis.png')" alt="Compromís" /></a></li>
+        <li class="podemos"><a href=""><img src="@asset('images/podemos.png')" alt="Podemos" /></a></li>
+      </ul>
+    </div>
+
+    <div class="hashtag elem-hidden">
+      <h1><a href="">#FinançamentJust</a></h1>
+    </div>
+
+    <div class="menu">
+      <div class="menu-items">
+        <ul class="d-flex d-lg-flex">
+          <li class="d-lg-none">
+            <a href="">Manifestació</a>
+          </li>
+          <li class="d-lg-none">
+            <a href="">Manifest</a>
+          </li>
+          <li class="d-lg-none">
+            <a href="">Notes de premsa</a>
+          </li>
+          <li class="d-lg-none">
+            <a href="">Contacta</a>
+          </li>
+          <li>
+            <div class="fb-like" data-href="http://financamentjust.com" data-layout="button_count" data-action="like" data-size="large" data-show-faces="false" data-share="false"></div>
+          </li>
+          <li>
+            <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-text="dfgfdg" data-url="http://fincancamentjust.com" data-hashtags="FinançamentJust" data-show-count="false">Tweet</a>
+          </li>
+          <li class="active">
+            <a href="">Valencià</a>
+          </li>
+          <li>
+            <a href="">Castellano</a>
+          </li>
+        </ul>
+      </div>
+      <ul class="menu-trigger d-lg-none">
+        <li><a href=""><i class="far fa-bars"></i> <span class="sr-only">Menú</span></a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
