ca26bd16bd75bea00ebdfb7022985d76805fff5a
Guillem <guillem@disedit.com>
2017-10-13 18:08:55 +0200

    Add metro line figure

---
+++ resources/views/partials/metro.blade.php
@@ -0,0 +1,12 @@
+<div class="metro-line">
+  <ul>
+    <li><div class="icon"><i class="far fa-euro-sign"></i></div> <span>Inversions</span></li>
+    <li><div class="icon"><i class="far fa-book"></i></div> <span>Educació</span></li>
+    <li><div class="icon"><i class="far fa-heartbeat"></i></div> <span>Sanitat</span></li>
+    <li><div class="icon"><i class="far fa-suitcase"></i></div> <span>Ocupació</span></li>
+    <li><div class="icon"><i class="far fa-wheelchair"></i></div> <span>Dependència</span></li>
+    <li><div class="icon"><i class="far fa-microchip"></i></div> <span>Investigació</span></li>
+    <li><div class="icon"><i class="far fa-handshake"></i></div> <span>Justícia social</span></li>
+    <li><div class="icon"><i class="far fa-train"></i></div> <span>Vertebració</span></li>
+  </ul>
+</div>
