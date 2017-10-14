2506a34df137ad716290082ec0b37dd5133e8f20
Guillem <guillem@disedit.com>
2017-10-14 20:01:10 +0200

    Typography for pages and posts

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -3,12 +3,12 @@
 @section('content')
   @while(have_posts()) @php(the_post())
     <div class="row">
-      <div class="col-lg-3 col-sm-4">
+      <div class="col-lg-2 col-sm-3 page-title">
         <div class="module module-section-title">
-          @php(the_title())
+          <h2>@php(the_title())</h2>
         </div>
       </div>
-      <div class="col-lg-6 col-sm-8">
+      <div class="col-lg-7 col-sm-9 post-content">
         <div class="module module-content" id="page-content">
           @include('partials.content-page')
         </div>
