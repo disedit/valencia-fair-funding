c1018854ec092eedd539410230b9bbcf28fe32a5
Guillem <guillem@disedit.com>
2017-10-15 11:09:17 +0200

    Add archive pages

---
+++ resources/views/front-page.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="row">
+    <div class="col-lg-3 col-md-4 demonstration">
+      @include('partials.modules.demonstration')
+    </div>
+
+    <div class="col-lg-6 col-md-8 manifesto">
+      @include('partials.modules.manifesto')
+    </div>
+
+    <div class="col-lg-3 sign-share">
+      <div class="row">
+        <div class="col-md-6 col-lg-12">
+          @include('partials.modules.sign')
+        </div>
+        <div class="col-md-6 col-lg-12">
+          @include('partials.modules.share')
+        </div>
+      </div>
+    </div>
+  </div>
+
+  @include('partials.modules.articles')
+@endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,20 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="row">
-    <div class="col-lg-3 col-md-4 demonstration">
-      @include('partials.modules.demonstration')
+  @if (!have_posts())
+    <div class="alert alert-warning">
+      {{ __('Sorry, no results were found.', 'sage') }}
     </div>
+    {!! get_search_form(false) !!}
+  @endif
 
-    <div class="col-lg-6 col-md-8 manifesto">
-      @include('partials.modules.manifesto')
-    </div>
+  @while (have_posts()) @php(the_post())
+    @include('partials.content-'.get_post_type())
+  @endwhile
 
-    <div class="col-lg-3 sign-share">
-      @include('partials.modules.sign')
-      @include('partials.modules.share')
-    </div>
-  </div>
-
-  @include('partials.modules.articles')
+  {!! get_the_posts_navigation() !!}
 @endsection
