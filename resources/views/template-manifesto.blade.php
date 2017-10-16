c33e9d74ba632056528213f9cfc18359ead66579
Guillem <guillem@disedit.com>
2017-10-16 17:06:14 +0200

    Add motion document to manifesto

--- resources/views/template-manifesto.blade.php
+++ resources/views/template-manifesto.blade.php
@@ -7,22 +7,9 @@
 @section('content')
   @while(have_posts()) @php(the_post())
     <div class="row manifesto">
-      <div class="col-lg-3 col-sm-4 manifesto-sidebar">
-        <div class="module module-page-title">
-          <h2>Adhereix-te</h2>
-        </div>
-
-        <div class="module module-page-title">
-          <h2>Moció</h2>
-        </div>
-
-        <div class="d-none d-sm-block">
-          @include('partials.modules.share')
-        </div>
-      </div>
-      <div class="col-lg-6 col-sm-8 post-content">
+      <div class="col-lg-6 col-sm-8 main-column">
         <div class="module">
-          <div class="module-content" id="page-content">
+          <div class="module-content post-content" id="page-content">
             <h2>@php(the_title())</h2>
 
             <div class="manifesto-content">
@@ -38,7 +25,35 @@
             </div>
           </div>
         </div>
-        @include('partials.modules.sign')
+        <div id="sign">
+          @include('partials.modules.sign')
+        </div>
+      </div>
+      <div class="col-lg-3 col-sm-4 manifesto-sidebar">
+        <div class="module module-page-title motion">
+          <div class="d-none d-sm-block">
+            <h2>Adhereix-te</h2>
+            <div class="mt-4">
+              <a href="#sign" class="btn btn-lg btn-primary btn-sign btn-block"><i class="far fa-pencil-alt"></i> Signa el manifest</a>
+            </div>
+          </div>
+
+          <hr class="my-4" />
+
+          <h2>Moció</h2>
+
+          <div class="attachments">
+            <p>Descarrega la moció que ... </p>
+            <ul>
+              <li><a href=""><i class="far fa-file"></i> Moció en valencià <abbr>PDF</abbr></a></li>
+              <li><a href=""><i class="far fa-file"></i> Moción en castellano <abbr>PDF</abbr></a></li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="d-none d-sm-block">
+          @include('partials.modules.share')
+        </div>
       </div>
       <div class="col-lg-3 sidebar">
         <div class="module module-signatures">
