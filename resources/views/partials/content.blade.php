a0d1167e9a04c539e9743478f08430e8455be467
Guillem <guillem@disedit.com>
2017-10-16 11:22:23 +0200

    Add thumbnail to articles

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -1,6 +1,9 @@
 <a href="{{ get_permalink() }}" class="col-md-6 col-xl-3 article-item article-{{ $i }}">
   <article @php(post_class())>
     <header>
+      <div class="thumbnail">
+        @php(the_post_thumbnail())
+      </div>
       @include('partials/entry-meta')
       <h2 class="entry-title">{{ get_the_title() }}</h2>
       <div class="icon"><i class="far fa-plus"></i></div>
