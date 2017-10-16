e0a6eae770afaddc0c6dcef8a7e943727370b010
Guillem <guillem@disedit.com>
2017-10-16 11:32:46 +0200

    Move organizers to footer

--- resources/views/partials/modules/contact.blade.php
+++ resources/views/partials/modules/contact.blade.php
@@ -1,3 +1,11 @@
 <div class="module module-contact">
-  Contact
+  <div class="organizers">
+    <ul>
+      <li class="ugt"><a href=""><img src="@asset('images/ugt.png')" alt="UGT" /></a></li>
+      <li class="ccoo"><a href=""><img src="@asset('images/ccoo.png')" alt="CCOO" /></a></li>
+      <li class="psoe"><a href=""><img src="@asset('images/psoe.png')" alt="PSOE" /></a></li>
+      <li class="compromis"><a href=""><img src="@asset('images/compromis.png')" alt="Compromís" /></a></li>
+      <li class="podemos"><a href=""><img src="@asset('images/podemos.png')" alt="Podemos" /></a></li>
+    </ul>
+  </div>
 </div>
