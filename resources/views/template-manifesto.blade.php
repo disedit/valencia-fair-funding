e106918e97b1b09cdb449c00cbf913955dc34cc5
Guillem <guillem@disedit.com>
2017-10-16 16:37:57 +0200

    Color changes and other refinements

--- resources/views/template-manifesto.blade.php
+++ resources/views/template-manifesto.blade.php
@@ -7,25 +7,35 @@
 @section('content')
   @while(have_posts()) @php(the_post())
     <div class="row manifesto">
-      <div class="col-lg-2 col-sm-4 manifesto-sidebar">
+      <div class="col-lg-3 col-sm-4 manifesto-sidebar">
         <div class="module module-page-title">
-          <h2><i class="far fa-file-alt"></i> @php(the_title())</h2>
+          <h2>Adhereix-te</h2>
+        </div>
+
+        <div class="module module-page-title">
+          <h2>Moció</h2>
         </div>
 
         <div class="d-none d-sm-block">
           @include('partials.modules.share')
         </div>
       </div>
-      <div class="col-lg-7 col-sm-8 post-content">
-        <div class="module module-content" id="page-content">
-          @include('partials.content-page')
+      <div class="col-lg-6 col-sm-8 post-content">
+        <div class="module">
+          <div class="module-content" id="page-content">
+            <h2>@php(the_title())</h2>
 
-          <div class="attachments">
-            <h4>{{ __('Documents adjunts', 'fair-funding') }}</h4>
-            <ul>
-              <li><a href=""><i class="far fa-file"></i> Manifest en valencià <abbr>PDF</abbr></a></li>
-              <li><a href=""><i class="far fa-file"></i> Manifiesto en castellano <abbr>PDF</abbr></a></li>
-            </ul>
+            <div class="manifesto-content">
+              @include('partials.content-page')
+            </div>
+
+            <div class="attachments">
+              <h4>{{ __('Documents adjunts', 'fair-funding') }}</h4>
+              <ul>
+                <li><a href=""><i class="far fa-file"></i> Manifest en valencià <abbr>PDF</abbr></a></li>
+                <li><a href=""><i class="far fa-file"></i> Manifiesto en castellano <abbr>PDF</abbr></a></li>
+              </ul>
+            </div>
           </div>
         </div>
         @include('partials.modules.sign')
