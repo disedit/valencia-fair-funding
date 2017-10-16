19fc760e6a6b3e75bda92101fe982a54e70a5ccf
Guillem <guillem@disedit.com>
2017-10-16 17:59:01 +0200

    Add contact info

--- resources/views/partials/modules/contact.blade.php
+++ resources/views/partials/modules/contact.blade.php
@@ -1,11 +1,22 @@
 <div class="module module-contact">
-  <div class="organizers">
-    <ul>
-      <li class="ugt"><a href=""><img src="@asset('images/ugt.png')" alt="UGT" /></a></li>
-      <li class="ccoo"><a href=""><img src="@asset('images/ccoo.png')" alt="CCOO" /></a></li>
-      <li class="psoe"><a href=""><img src="@asset('images/psoe.png')" alt="PSOE" /></a></li>
-      <li class="compromis"><a href=""><img src="@asset('images/compromis.png')" alt="Compromís" /></a></li>
-      <li class="podemos"><a href=""><img src="@asset('images/podemos.png')" alt="Podemos" /></a></li>
-    </ul>
+  <div class="row">
+    <div class="col-md-6">
+      <div class="organizers">
+        <h4>{{ __('Organitzen', 'fair-funding') }}</h4>
+        <ul>
+          <li class="ugt"><a href=""><img src="@asset('images/ugt.png')" alt="UGT" /></a></li>
+          <li class="ccoo"><a href=""><img src="@asset('images/ccoo.png')" alt="CCOO" /></a></li>
+          <li class="psoe"><a href=""><img src="@asset('images/pspv.jpg')" alt="PSPV-PSOE" /></a></li>
+          <li class="compromis"><a href=""><img src="@asset('images/compromis.png')" alt="Compromís" /></a></li>
+          <li class="podemos"><a href=""><img src="@asset('images/podemos.png')" alt="Podemos" /></a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="info">
+        <h4>{{ __('Contacta', 'fair-funding') }}</h4>
+        <a href="">info@finançamentjust.com</a>
+      </div>
+    </div>
   </div>
 </div>
