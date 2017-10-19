1db0e120f97526f9b66d1741a8367383b6380544
Guillem <guillem@disedit.com>
2017-10-19 18:28:19 +0200

    Make metro line translatable

--- resources/views/partials/modules/metro.blade.php
+++ resources/views/partials/modules/metro.blade.php
@@ -1,13 +1,13 @@
 <div class="metro-line">
   <ul>
-    <li><div class="icon"><i class="far fa-euro-sign"></i></div> <span>Inversions</span></li>
-    <li><div class="icon"><i class="far fa-book"></i></div> <span>Educació</span></li>
-    <li><div class="icon"><i class="far fa-heartbeat"></i></div> <span>Sanitat</span></li>
-    <li><div class="icon"><i class="far fa-suitcase"></i></div> <span>Ocupació</span></li>
-    <li><div class="icon"><i class="far fa-wheelchair"></i></div> <span>Dependència</span></li>
-    <li><div class="icon"><i class="far fa-microchip"></i></div> <span>Investigació</span></li>
-    <li><div class="icon"><i class="far fa-handshake"></i></div> <span>Justícia social</span></li>
-    <li><div class="icon"><i class="far fa-train"></i></div> <span>Vertebració</span></li>
+    <li><div class="icon"><i class="far fa-euro-sign"></i></div> <span><?php pll_e('Inversions', 'fair-funding'); ?></span></li>
+    <li><div class="icon"><i class="far fa-book"></i></div> <span><?php pll_e('Educació', 'fair-funding'); ?></span></li>
+    <li><div class="icon"><i class="far fa-heartbeat"></i></div> <span><?php pll_e('Sanitat', 'fair-funding'); ?></span></li>
+    <li><div class="icon"><i class="far fa-suitcase"></i></div> <span><?php pll_e('Ocupació', 'fair-funding'); ?></span></li>
+    <li><div class="icon"><i class="far fa-wheelchair"></i></div> <span><?php pll_e('Dependència', 'fair-funding'); ?></span></li>
+    <li><div class="icon"><i class="far fa-microchip"></i></div> <span><?php pll_e('Investigació', 'fair-funding'); ?></span></li>
+    <li><div class="icon"><i class="far fa-handshake"></i></div> <span><?php pll_e('Justícia social', 'fair-funding'); ?></span></li>
+    <li><div class="icon"><i class="far fa-train"></i></div> <span><?php pll_e('Vertebració', 'fair-funding'); ?></span></li>
     <li></li>
   </ul>
 </div>
