5ef63ea523f27ae2a0e3fbe17bd3cb82117b3026
Guillem <guillem@disedit.com>
2017-10-19 10:40:04 +0200

    Add b/w logos for Podemos, PSOE and UGT

--- resources/views/partials/modules/contact.blade.php
+++ resources/views/partials/modules/contact.blade.php
@@ -4,11 +4,11 @@
       <div class="organizers">
         <h4>{{ __('Organitzen', 'fair-funding') }}</h4>
         <ul>
-          <li class="ugt"><a href=""><img src="@asset('images/ugt.bw.png')" alt="UGT" /></a></li>
-          <li class="ccoo"><a href=""><img src="@asset('images/ccoo.bw.png')" alt="CCOO" /></a></li>
-          <li class="psoe"><a href=""><img src="@asset('images/pspv.bw.png')" alt="PSPV-PSOE" /></a></li>
-          <li class="compromis"><a href=""><img src="@asset('images/compromis.bw.png')" alt="Compromís" /></a></li>
-          <li class="podemos"><a href=""><img src="@asset('images/podemos.bw.png')" alt="Podemos" /></a></li>
+          <li class="ugt"><a href="http://www.ugt-pv.es" target="_blank" rel="noopener"><img src="@asset('images/ugt.bw.png')" alt="UGT" /></a></li>
+          <li class="ccoo"><a href="http://www.pv.ccoo.es/" target="_blank" rel="noopener"><img src="@asset('images/ccoo.bw.png')" alt="CCOO" /></a></li>
+          <li class="psoe"><a href="http://www.pspvpsoe.net/" target="_blank" rel="noopener"><img src="@asset('images/pspv.bw.png')" alt="PSPV-PSOE" /></a></li>
+          <li class="compromis"><a href="https://compromis.net" target="_blank" rel="noopener"><img src="@asset('images/compromis.bw.png')" alt="Compromís" /></a></li>
+          <li class="podemos"><a href="https://cvalenciana.podemos.info/" target="_blank" rel="noopener"><img src="@asset('images/podemos.bw.png')" alt="Podemos" /></a></li>
         </ul>
       </div>
     </div>
