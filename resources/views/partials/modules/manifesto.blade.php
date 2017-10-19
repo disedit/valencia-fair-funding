54eb87fea1cfcbbdb2916c1de5fa0a210ec72d21
Guillem <guillem@disedit.com>
2017-10-19 09:29:50 +0200

    Add shortened manifesto on front page

--- resources/views/partials/modules/manifesto.blade.php
+++ resources/views/partials/modules/manifesto.blade.php
@@ -1,15 +1,12 @@
+@php(the_post())
+@php
+  the_post();
+  $permalink = App::getPermalinkInLanguage('manifest');
+@endphp
 <div class="module module-manifesto">
   <div class="module-content">
-    @php
-      global $post;
-      $manifesto = get_page_by_path('manifest');
-      $page = pll_get_post($manifesto->ID);
-      $post = get_post($page);
-      setup_postdata($post);
-    @endphp
-
     <div class="col-sm-6">
-      <a href="@php(the_permalink())" class="manifesto-title"><h2>@php(the_title())</h2></a>
+      <a href="{{ $permalink }}" class="manifesto-title"><h2><i class="far fa-file-alt"></i> {{ __('Manifest', 'fair-funding') }}</h2></a>
     </div>
 
     <div class="manifesto-content">
@@ -18,11 +15,9 @@
 
     <hr aria-hidden="true" />
 
-    <a href="@php(the_permalink())" data-title="@php(the_title())" data-container="page-content" class="btn btn-light btn-lg btn-block load-modal">
+    <a href="{{ $permalink }}" data-title="@php(the_title())" data-container="page-content" class="btn btn-light btn-lg btn-block">
       <span class="text">{{ __('Continua llegint...', 'fair-funding') }}</span>
       <span class="icon"><i class="far fa-chevron-right"></i></span>
     </a>
   </div>
 </div>
-
-@php(wp_reset_postdata())
