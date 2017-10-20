61f5f7747ed45999fbc719a07508d1c0efac325f
Guillem <guillem@disedit.com>
2017-10-20 10:18:16 +0200

    Add wp nav menu

--- app/helpers.php
+++ app/helpers.php
@@ -138,3 +138,27 @@ function display_sidebar()
     isset($display) || $display = apply_filters('sage/display_sidebar', false);
     return $display;
 }
+
+/**
+ * Return a WP menu without <ul>s
+ * @return string
+ */
+function wp_nav_menu_no_ul()
+{
+    $options = array(
+        'echo' => false,
+        'container' => false,
+        'theme_location' => 'primary_navigation',
+        'fallback_cb'=> 'default_page_menu'
+    );
+
+    $menu = wp_nav_menu($options);
+    echo preg_replace(array(
+        '#^<ul[^>]*>#',
+        '#</ul>$#'
+    ), '', $menu);
+}
+
+function default_page_menu() {
+   wp_list_pages('title_li=');
+}
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -11,20 +11,9 @@
     <div class="menu">
       <div class="menu-items">
         <ul class="d-flex d-lg-flex">
-          <li class="d-lg-none">
-            <a href="">Manifestació</a>
-          </li>
-          <li class="d-lg-none">
-            <a href="">Manifest</a>
-          </li>
-          <li class="d-lg-none">
-            <a href="">Notes de premsa</a>
-          </li>
-          <li class="d-lg-none">
-            <a href="">Contacta</a>
-          </li>
+          {{ App\wp_nav_menu_no_ul() }}
           <li>
-            <div class="fb-like" data-href="{{ home_url('/') }}" data-layout="button_count" data-action="like" data-size="large" data-show-faces="false" data-share="false"></div>
+            <div class="fb-like" data-href="{{ urlencode('https://finançamentjust.com') }}" data-layout="button_count" data-action="like" data-size="large" data-show-faces="false" data-share="false"></div>
           </li>
           <li>
             <a href="https://twitter.com/share?ref_src={{ home_url('/') }}" class="twitter-share-button" data-size="large" data-text="<?php pll_e('Tweet', 'fair-funding') ?>" data-url="{{ home_url('/') }}" data-hashtags="FinançamentJust" data-show-count="false">Tweet</a>
