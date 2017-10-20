496f814690801c6a417dc920e8fc8448fd614209
Guillem <guillem@disedit.com>
2017-10-20 11:16:03 +0200

    Pull download links from content

--- resources/views/partials/modules/downloads.blade.php
+++ resources/views/partials/modules/downloads.blade.php
@@ -1,19 +1,24 @@
+@php
+  global $post;
+  $downloads = get_page_by_path('descarregues');
+  $page = pll_get_post($downloads->ID);
+  $post = get_post($page);
+  setup_postdata($post);
+@endphp
 <div class="module module-downloads d-none d-md-block">
   <div class="module-icon">
     <i class="far fa-cloud-download-alt" aria-hidden="true"></i>
   </div>
   <div class="module-content">
     <div class="module-header">
-      <h2><i class="far fa-cloud-download-alt" aria-hidden="true"></i> <?php pll_e('Descàrregues', 'fair-funding') ?></h2>
+      <h2><i class="far fa-cloud-download-alt" aria-hidden="true"></i> {{ get_the_title() }}</h2>
     </div>
 
     <hr />
 
     <div class="mt-4">
-      <a href="" class="btn btn-lg btn-block btn-outline-light"><i class="far fa-file-image"></i> <?php pll_e('Cartell', 'fair-funding') ?></a>
-      <a href="" class="btn btn-lg btn-block btn-outline-light"><i class="far fa-file-alt"></i> <?php pll_e('Manifest', 'fair-funding') ?> <abbr>A5</abbr></a>
-      <a href="" class="btn btn-lg btn-block btn-outline-light"><i class="far fa-file-alt"></i> <?php pll_e('Moció', 'fair-funding') ?></a>
-      <a href="" class="btn btn-lg btn-block btn-outline-light"><i class="far fa-file-image"></i> <?php pll_e('Avatar xarxes', 'fair-funding') ?></a>
+      @php(the_content())
     </div>
   </div>
 </div>
+@php(wp_reset_postdata())
